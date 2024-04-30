9aae29e2cd5855fdf43827a0debf5b0a4713e6d5
fan <xiaofanms@outlook.com>
2024-04-30 09:52:25 +0800

    完善。

--- src/app/strategy/TestRouteReuseStrategy.ts
+++ src/app/strategy/TestRouteReuseStrategy.ts
@@ -1,29 +1,44 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy, BaseRouteReuseStrategy } from "@angular/router";
+import { ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy, BaseRouteReuseStrategy, Route } from "@angular/router";
 
 @Injectable()
 export class TestRouteReuseStrategy extends RouteReuseStrategy {
 
+  private cache: Map<Route, DetachedRouteHandle | null> = new Map();
+
   override shouldDetach(route: ActivatedRouteSnapshot): boolean {
     console.log('shouldDetach');
     console.log(route);
-    return false;
+    return null != route.routeConfig;
   }
+
   override store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle | null): void {
     console.log('store');
     console.log(route);
     console.log(handle);
+    if (route.routeConfig) {
+      this.cache.set(route.routeConfig, handle);
+    }
   }
+
   override shouldAttach(route: ActivatedRouteSnapshot): boolean {
     console.log('shouldAttach');
     console.log(route);
+    if (route.routeConfig && this.cache.has(route.routeConfig)) {
+      return true;
+    }
     return false;
   }
+
   override retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle | null {
     console.log('retrieve');
     console.log(route);
-    return null;
+    if (null == route.routeConfig || !this.cache.has(route.routeConfig)) {
+      return null;
+    }
+    return this.cache.get(route.routeConfig) as DetachedRouteHandle | null;
   }
+
   override shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
     console.log('shouldReuseRoute');
     console.log(curr);
@@ -31,6 +46,7 @@ export class TestRouteReuseStrategy extends RouteReuseStrategy {
     console.log(future.routeConfig?.path);
     return future.routeConfig === curr.routeConfig;
   }
+
 }
 // @Injectable()
 // export class TestRouteReuseStrategy extends BaseRouteReuseStrategy {}
